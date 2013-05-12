d34ab3e5cd777c5cbb9fb53d8d5c6d2d63c187a8
Rui Lopes <ruidlopes@gmail.com>
2013-05-11 20:30:58 -0700

    Improved playback visual cue

--- js/main.js
+++ js/main.js
@@ -526,15 +526,14 @@ loop.audio.Looper.prototype.render = function() {
     var left = sample.leftAverage;
     var right = sample.rightAverage;
 
-    lib.ui.ctx.beginPath();
-    lib.ui.ctx.moveTo(i, middle - (Math.abs(left * 20) * middle));
-    lib.ui.ctx.lineTo(i, middle + (Math.abs(right * 20) * middle));
-    lib.ui.ctx.closePath();
-
     var viewportI = this.viewportTranslateX(i);
+    var lineMinY = middle - (Math.abs(left * 20) * middle);
+    var lineMaxY = middle + (Math.abs(left * 20) * middle);
 
-    if (this.isPlaying && viewportI >= this.selectionMin && viewportI <= this.playerPosition) {
+    if (this.isPlaying && viewportI == this.playerPosition) {
       // Playing
+      lineMinY = 0;
+      lineMaxY = this.rect.height;
       lib.ui.ctx.strokeStyle = '#c00';
     } else if (this.selectionMin != this.selectionMax &&
         viewportI >= this.selectionMin && viewportI <= this.selectionMax) {
@@ -544,6 +543,11 @@ loop.audio.Looper.prototype.render = function() {
       // Recording
       lib.ui.ctx.strokeStyle = '#69c';
     }
+
+    lib.ui.ctx.beginPath();
+    lib.ui.ctx.moveTo(i, lineMinY);
+    lib.ui.ctx.lineTo(i, lineMaxY);
+    lib.ui.ctx.closePath();
     lib.ui.ctx.stroke();
   }
 
